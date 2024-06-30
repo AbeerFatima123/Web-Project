e2f58f20a95df37cbe60a2be222082856e880d60
fayyez <fayyez2056@gmail.com>
2024-06-30 21:09:44 +0500

    added dashboard navigation

--- index.js
+++ index.js
@@ -1,11 +1,79 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+
+// Attach the public directory to our express server
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
 // const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.sendFile('/pages/sample.html', { root: __dirname });
+    res.redirect('/homepage');
 });
 
+//login page
+app.get('/login', (req, res) => {
+    res.sendFile('/pages/login page.html', { root: __dirname });
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    // login logic here
+    res.redirect('/dashboard');
+});
+
+// sign up page
+app.get('/signup', (req, res) => {
+    res.sendFile('/pages/signup page.html', { root: __dirname });
+});
+
+// homepage
+app.get('/homepage', (req, res) => {
+    res.sendFile('/pages/homepage.html', { root: __dirname });
+});
+
+/// Admin dashboard ///
+app.get('/dashboard', (req, res) => {
+    res.sendFile('/pages/dashboard_Page.html', { root: __dirname });
+});
+
+// shop setup
+app.get('/dashboard_shopSetup', (req, res) => {
+    res.sendFile('/pages/shop setup Page.html', { root: __dirname });
+});
+
+app.get('/dashboard_shopSetup_integration', (req, res) => {
+    res.sendFile('/pages/setup_integrations.html', { root: __dirname });
+});
+
+app.get('/dashboard_shopSetup_team', (req, res) => {
+    res.sendFile('/pages/setup_Team.html', { root: __dirname });
+});
+
+// projects
+app.get('/dashboard_project', (req, res) => {
+    res.sendFile('/pages/project page.html', { root: __dirname });
+});
+
+// clients
+app.get('/dashboard_clients', (req, res) => {
+    res.sendFile('/pages/client page.html', { root: __dirname });
+});
+
+// orders
+app.get('/dashboard_orders', (req, res) => {
+    res.sendFile('/pages/orders Page.html', { root: __dirname });
+});
+
+// notifications
+app.get('/dashboard_notifications', (req, res) => {
+    res.sendFile('/pages/Notifications Page.html', { root: __dirname });
+});
+
+/// Super Admin dashboard ///
+
+// listen for requests
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
--- utilis/dashboard.js
+++ utilis/dashboard.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var filtersButton = document.getElementById('filters-button');
     var filtersDropdown = document.getElementById('filters-dropdown');
-  
+
     filtersButton.addEventListener('click', function() {
       // Toggle the display of the dropdown menu
       filtersDropdown.style.display = filtersDropdown.style.display === 'block' ? 'none' : 'block';
     });
-  
+
     // Close dropdown if user clicks outside of it
     document.addEventListener('click', function(event) {
       if (!filtersDropdown.contains(event.target) && event.target !== filtersButton) {
@@ -14,4 +14,4 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
-  
+
